f0589131bd8c58349b69cd650b784b631bce237d
David Rheinbay <david.rheinbay@gmail.com>
2019-02-20 18:47:48 +0100

    Airbnb in Beautiful

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,15 +1,19 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[edit update show destroy]
+
   def index
+    if params[:search].nil?
     @flats = Flat.all
+    else
+    @flats = Flat.where("name LIKE '%#{params[:search]}%'")
+    end
   end
 
   def new
     @flat = Flat.new
   end
 
-  def show
-    @flat = Flat.find(params[:id])
-  end
+  def show; end
 
   def create
     @flat = Flat.new(flat_params)
@@ -20,9 +24,29 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flats_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :number_of_guests, :price_per_night)
   end
+
+  def set_flat
+  @flat = Flat.find(params[:id])
+  end
+
 end
